6eba664ac5c33414c01ae8d37c48f411c4490ac1
Dave Thomas <davidcthomas@fb.com>
2020-02-07 20:07:16 -0800

    test mail

--- 1team/obj/Mail.php
+++ 1team/obj/Mail.php
@@ -8,20 +8,20 @@ include_once ('utils.php');
  */
 class Mail {
   function __construct( ) {
-    $this->from = new SendGrid\Email(null, emailadmin);
-    $this->apiKey = getenv('SENDGRID_API_KEY');
+    // $this->from = new SendGrid\Email(null, emailadmin);
+    // $this->apiKey = getenv('SENDGRID_API_KEY');
   }
 
   function mail($to_address, $subject, $body){
-    $from = $this->from;
-
-    $to = new SendGrid\Email(null, $to_address);
-    $content = new SendGrid\Content("text/plain", $body);
-    $mail = new SendGrid\Mail($from, $subject, $to, $content);
-
-    $sg = new \SendGrid($this->apiKey);
-    $response = $sg->client->mail()->send()->post($mail);
-    return $response->statusCode();
+    // $from = $this->from;
+    //
+    // $to = new SendGrid\Email(null, $to_address);
+    // $content = new SendGrid\Content("text/plain", $body);
+    // $mail = new SendGrid\Mail($from, $subject, $to, $content);
+    //
+    // $sg = new \SendGrid($this->apiKey);
+    // $response = $sg->client->mail()->send()->post($mail);
+    // return $response->statusCode();
     // echo $response->headers();
     // echo $response->body();
   }
